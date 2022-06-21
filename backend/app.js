852aba5a7eaddb789821ac76a68faa9dc0ddd196
subham-kumar-au17 <subham4663@gmail.comq>
2022-06-21 11:26:26 +0530

    JWT authentication

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const errorMiddelware = require('./middleware/error')
 
-// const path = require('path');
+const path = require('path');
 
 app.use(express.json())
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 const userSchema = new mongoose.Schema({
@@ -45,6 +46,8 @@ const userSchema = new mongoose.Schema({
     resetPasswordExpire: Date
 })
 
+/*<--password hashing-->*/
+
 userSchema.pre ('save', async function(next){
     if(!this.isModified('password')){
         next();
@@ -52,5 +55,14 @@ userSchema.pre ('save', async function(next){
 
     this.password = await bcrypt.hash(this.password, 10);
 })
+
+/*<--jwt token-->*/
+
+userSchema.methods.getJWTToken = function() {
+    return jwt.sign({id:this.id}, process.env.JWT_SECRET, {
+        expiresIn:process.env.JWT_EXPIRE,
+    })
+}
+
 
 module.exports = mongoose.model('User', userSchema)
