99607af844679587c4b80210fd889f0f3a0e6a19
subham-kumar-au17 <subham4663@gmail.comq>
2022-06-23 00:01:02 +0530

    admin-auth

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,7 +4,7 @@ const { isAuthenticatedUser, admin } = require('../middleware/authentication');
 const router = express.Router();
 
 
-router.route('/products').get(isAuthenticatedUser, getAllProducts);
+router.route('/products').get(getAllProducts);
 router.route('/product/new').post(isAuthenticatedUser, admin("admin"), createProduct);
 router.route('/product/:id').put(isAuthenticatedUser, admin("admin"), updateProduct);
 router.route('/product/:id').delete(isAuthenticatedUser, admin("admin"), deleteProduct);
