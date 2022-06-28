45aa452c0fb58f43094e12e70eda05c32f3c44dc
subham-kumar-au17 <subham4663@gmail.comq>
2022-06-28 23:40:52 +0530

    forgot mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,6 +3,8 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
 
     const transporter = nodeMailer.createTransport({
+        host: process.env.SMPT_HOST,
+        port: process.env.SMPT_PORT,
         service: process.env.SMPT_SERVICE,
         auth: {
             user: process.env.SMPT_EMAIL,
